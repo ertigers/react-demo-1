7daa56defa83b11518ab46b67fb4309128396292
yihu-lv <1720427782@qq.com>
2021-05-12 19:52:57 +0800

    shouldComponentUpdate，PureComponent

---
+++ src/App3.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      n:1
+    }
+  }
+  onClick = () => {
+    this.setState(state=>({
+      n: state.n + 1
+    }))
+    // this.setState(state=>({
+    //   n: state.n - 1
+    // }))
+  }
+  // shouldComponentUpdate(newProps,newState) {
+  //   if(newState.n === this.state.n) {
+  //     return false
+  //   }else {
+  //     return true
+  //   }
+  // }
+  render() {
+    console.log("render了一次");
+    return(
+      <div>
+        {this.state.n}
+        <button onClick={this.onClick}>+1</button>
+      </div>
+    )
+  }
+}
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App2.js'
+import App from './App3.js'
 ReactDOM.render(<App/>,document.getElementById('root'));
